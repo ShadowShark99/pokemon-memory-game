a31c69f61a9fc06369e9fe56cc29b4d1e558f4c0
ShadowShark99 <kadenwchan@gmail.com>
2025-07-08 19:54:15 -0500

    Implement High Score useEffect hook

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props{
   score: number;
 }
 
 const ScoreBoard = ({score}: Props) => {
+  const [high, setHigh] = useState(0);
+  useEffect(() => {
+    if(score > high)
+      setHigh(score);
+
+
+  }, [score]);
+
   return (
-    <div>Score: {score}</div>
+    <>
+      <div>Score: {score}</div>
+      <div>HighScore: {high}</div>
+    </>
+    
   )
 }
 
