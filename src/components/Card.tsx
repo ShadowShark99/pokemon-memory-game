da0bcabb6541b2b686a6e3df03a2a92dcbb492d0
ShadowShark99 <kadenwchan@gmail.com>
2025-07-08 20:22:33 -0500

    Clean for production build

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import "../styles/Card.css";
 interface Props{
   pokeId: number;
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Card from './Card'
 import "../styles/Game.css";
 import Scramble from './Scramble';
